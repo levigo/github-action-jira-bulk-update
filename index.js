cb32fc5a3de2aab978994166905d047d813a8e06
David Werth <d.werth@levigo.de>
2025-09-09 06:45:17 +0200

    Update to not use deprecated API

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const core = require("@actions/core");
-const JiraApi = require("jira-client")
+const core = require('@actions/core');
+// const core = require("./mockCore.js");
+const requestHelper = require('./requestHelper.js');
 
-let jira, domain, username, password, jql, fieldName, fieldValue, appendValue;
+let domain, username, password, jql, fieldName, fieldValue, appendValue;
 (async () => {
     try {
         domain = core.getInput("domain");
@@ -17,13 +18,6 @@ let jira, domain, username, password, jql, fieldName, fieldValue, appendValue;
             return;
         }
 
-        // Initialize
-        jira = new JiraApi({
-            protocol: "https",
-            host: domain,
-            username: username,
-            password: password,
-        });
         searchAndUpdate(jql, fieldName, fieldValue, appendValue)
     } catch (error) {
         core.setFailed(error.message);
@@ -32,14 +26,21 @@ let jira, domain, username, password, jql, fieldName, fieldValue, appendValue;
 
 async function searchAndUpdate(jql, fieldName, fieldValue, appendValue) {
     const issues = await searchIssues(jql);
-    for (let i = 0; i < issues.issues.length; i++) {
-        const issue = issues.issues[i];
+    for (let i = 0; i < issues.length; i++) {
+        const issue = issues[i];
         updateIssue(issue, fieldName, fieldValue, appendValue);
     }
 }
 
 async function searchIssues(jql) {
-    return await jira.searchJira(jql);
+    const retVal = [];
+    const issueRef = await requestHelper.getRequest(`/rest/api/3/search/jql?jql=${encodeURI(jql)}`);
+    for (let i = 0; i < issueRef.issues.length; i++) {
+        const issuePath = `/rest/api/3/issue/${issueRef.issues[i].id}`;
+        const issue = await requestHelper.getRequest(issuePath);
+        retVal.push(issue);
+    }
+    return retVal;
 }
 
 async function updateIssue(issue, fieldName, fieldValue, appendValue) {
@@ -48,7 +49,11 @@ async function updateIssue(issue, fieldName, fieldValue, appendValue) {
         // from e.g. TEST-1 get the project key --> TEST
         const projectKey = getProjectKey(issue.key);
         const projectId = await getProjectId(projectKey);
-        const version = await getVersion(projectId, fieldValue);
+        let version = await getVersion(projectId, fieldValue);
+        if (!version) {
+            await createVersion(issue, fieldValue, projectId);
+            version = await getVersion(projectId, fieldValue);
+        }
         return await addVersion(issue, version.id);
     } else {
         if (appendValue) {
@@ -57,16 +62,19 @@ async function updateIssue(issue, fieldName, fieldValue, appendValue) {
                 fieldValue = oldValue + ", " + fieldValue;
             }
         }
-        return await jira.updateIssue(issue.id, {
-            fields: {
+        const bodyData = {
+            "fields": {
                 [fieldName]: fieldValue
             }
-        });
+        }
+        const putUpdateIssuePath = `/rest/api/3/issue/${issue.id}`;
+        return await requestHelper.bodyRequest(putUpdateIssuePath, JSON.stringify(bodyData), 'PUT');
+
     }
 }
 
 async function getVersion(projectId, versionName) {
-    const versions = await jira.getVersions(projectId);
+    const versions = await requestHelper.getRequest(`/rest/api/3/project/${projectId}/versions`);
     for (let i = 0; i < versions.length; i++) {
         const version = versions[i];
         if (version.name === versionName) {
@@ -76,21 +84,39 @@ async function getVersion(projectId, versionName) {
     return undefined;
 }
 
+/**
+ * Make sure the version is not archived as it cannot be added to an issue if it is archived
+ * @param issue
+ * @param versionId
+ * @returns {Promise<void>}
+ */
 async function addVersion(issue, versionId) {
-    await jira.updateIssue(issue.id, {
+    const bodyData = {
         update: {
             fixVersions: [{"add": {id: versionId}}]
         }
-    });
+    }
+    const putUpdateIssuePath = `/rest/api/3/issue/${issue.id}`;
+    await requestHelper.bodyRequest(putUpdateIssuePath, JSON.stringify(bodyData), 'PUT');
+}
+
+async function createVersion(issue, versionName, projectId) {
+    const bodyData = {
+      "archived": false,
+      "description": "Created by GitHub Action Jira Bulk Update",
+      "name": versionName,
+      "projectId": projectId,
+      "released": false
+    };
+    const putUpdateIssuePath = `/rest/api/3/version`;
+    await requestHelper.bodyRequest(putUpdateIssuePath, JSON.stringify(bodyData), 'POST');
 }
 
 async function getProjectId(projectKey) {
-    const project = await jira.getProject(projectKey);
+    const project = await requestHelper.getRequest(`/rest/api/3/project/${projectKey}`);
     return project.id
 }
 
 function getProjectKey(issueKey) {
     return issueKey.substring(0, issueKey.indexOf("-"));
 }
-
-module.exports = searchAndUpdate;
